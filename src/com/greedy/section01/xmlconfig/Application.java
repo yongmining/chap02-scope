9abbbe36f1caf9eb965716e4ee948874804dc3fc
DESKTOP-S5B8BH8\tjoeun <gpsyo911@gamil.com>
2023-02-14 18:31:27 +0900

    chap scope

---
+++ src/com/greedy/section01/xmlconfig/Application.java
@@ -0,0 +1,15 @@
+package com.greedy.section01.xmlconfig;
+
+import static com.greedy.section01.xmlconfig.Template.getSqlSession;
+
+public class Application {
+
+	public static void main(String[] args) {
+		
+		System.out.println(getSqlSession());
+		System.out.println(getSqlSession());
+		System.out.println(getSqlSession());
+
+	}
+
+}
